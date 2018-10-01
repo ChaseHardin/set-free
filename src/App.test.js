89ac84620e91ff7f7d4977baf4ac1609aaf38894
Chase Hardin <chase.hardin@scribbletechnology.com>
2018-10-01 17:39:17 -0500

    Add iphone icons for progressive web app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+var assert = require('assert');
+
+configure({ adapter: new Adapter() });
+
+describe('App Component', () => {
+    it('should render app component', () => {
+        var component = mount(<App />);
+
+        assert.equal(component.find('App').length, 1);
+        assert.equal(component.find('.App-title').text(), 'Welcome to React');
+    });
+})
