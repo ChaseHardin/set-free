fda2d2599b17c1c269d2eab4216e3cef4ef2ca1e
Chase Hardin <chase.hardin@scribbletechnology.com>
2018-11-10 21:32:48 -0600

    Add test to verify logout is working correctly

--- cypress/integration/Login.spec.js
+++ cypress/integration/Login.spec.js
@@ -11,3 +11,23 @@ describe('Login with Firebase', () => {
         cy.get('.navbar-title').should('have.length', 0);
     });
 });
+
+describe('Logout with Firebase', () => {
+    beforeEach('log user into application', () => {
+        cy.visit('http://localhost:3000/');
+
+        cy.get('#emailInput')
+            .type('test@email.com');
+
+        cy.get('#passwordInput')
+            .type('test123');
+
+        cy.get('.btn-success').click();
+    })
+
+    it('should logout of the application', () => {
+        cy.get('.btn-danger').click();
+        
+        cy.get('.login-container').contains('Email Address');
+    });
+});
